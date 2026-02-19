ac9214e551b7370a383562d87911ce25a460c4bb
yongminari <easyid21c@gmail.com>
2026-02-19 10:59:01 +0900

    tmux autorun

--- nix/modules/shell.nix
+++ nix/modules/shell.nix
@@ -1,11 +1,10 @@
 { config, pkgs, lib, ... }:
 
 {
-  # 1. Starship 프롬프트 (테마 파일 로드)
+  # 1. Starship 프롬프트
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
-    # 같은 폴더의 starship.toml 파일을 읽어서 적용
     settings = lib.importTOML ./starship.toml;
   };
 
@@ -21,6 +20,7 @@
       plugins = [ "git" "virtualenv" "history-substring-search" ];
     };
 
+    # [여기가 핵심입니다]
     initContent = ''
       export PATH=$HOME/.local/bin:$PATH
       
@@ -34,6 +34,18 @@
       # History Search 키바인딩
       bindkey '^[[A' history-substring-search-up
       bindkey '^[[B' history-substring-search-down
+
+      # ---------------------------------------------------------
+      # [Tmux 자동 실행 설정]
+      # ---------------------------------------------------------
+      # 1. 대화형 쉘인지 확인 ($- string에 i가 포함되어 있는지)
+      # 2. 이미 Tmux 안이 아닌지 확인 ($TMUX 변수가 비어있어야 함)
+      # 3. VS Code 터미널이 아닌지 확인 ($TERM_PROGRAM != "vscode")
+      # ---------------------------------------------------------
+      if [[ $- == *i* ]] && [[ -z "$TMUX" ]] && [[ "$TERM_PROGRAM" != "vscode" ]]; then
+        # exec: 현재 쉘을 tmux로 '교체'합니다. (tmux 끄면 터미널도 꺼짐)
+        exec tmux
+      fi
     '';
   };
 }
